df6a41d6740ed9246d05917ba6c05aa1d9c51213
DenisGultsev <135717609+DenisGultsev@users.noreply.github.com>
2023-09-11 13:12:52 +1000

    Seminar One

---
+++ SeminarOne.py
@@ -0,0 +1,12 @@
+﻿# C#/C++/C/Java - статическая типизация данных
+# Python/JavaScript - динамическая типизация данных
+
+# int age = 27; -> []
+
+# age = 27 -> class 'int' -> []
+
+# Компилятор и Интерпритатор
+
+name = input("Введите свое имя: ")
+print(f'Hello, {name}!')
+print(type(name))
