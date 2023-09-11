ccc73854851653e358a37c50277b0adefb92d890
DenisGultsev <135717609+DenisGultsev@users.noreply.github.com>
2023-09-11 22:05:45 +1000

    Class

--- SeminarOne.py
+++ SeminarOne.py
@@ -57,11 +57,32 @@
 # При выполнении этой задачи нельзя пользоваться
 # условной инструкцией if и циклами.
 
-import math
+# import math
 
-n = 700
-m = 700
+# n = 700
+# m = 700
 
-days = math.ceil(m / n)
+# days = math.ceil(m / n)
 
-print(days)
+# print(days)
+
+# Задача № 2.
+# В какой-то школе решили взять три новых математических класса
+# и оборудовать кабинеты для них новыми партами.
+# За каждой партой могут сидеть два ученика.
+# Известно количество учащихся в каждом из трех классов.
+# Вы вводите меньшее количество деталей, которые необходимо приобрести для них.
+
+# import math
+
+# students_class1 = int(input("Введите количество парт в классе 1: "))
+# students_class2 = int(input("Введите количество парт в классе 2: "))
+# students_class3 = int(input("Введите количество парт в классе 3: "))
+
+# desks_class1 = math.ceil(students_class1 / 2)
+# desks_class2 = math.ceil(students_class2 / 2)
+# desks_class3 = math.ceil(students_class3 / 2)
+
+# total_desks = desks_class1 + desks_class2 + desks_class3
+
+# print(total_desks)
